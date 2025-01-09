cb36b44e8f040bd6d49d7a01d3ee7c9cc28a3540
devjaeyoon <devjaeyoon@gmail.com>
2025-01-09 19:13:51 +0900

    feat(Baekjoon): solve problem 24484 - 알고리즘 수업 - 깊이 우선 탐색 6

---
+++ Baekjoon/24484/solution.js
@@ -0,0 +1,42 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [N, M, R] = input[0].split(' ').map(Number);
+const adjacencyList = Array.from({ length: N + 1 }, () => []);
+
+for (let i = 1; i <= M; i++) {
+  const [u, v] = input[i].split(' ').map(Number);
+  adjacencyList[u].push(v);
+  adjacencyList[v].push(u);
+}
+
+for (let i = 1; i <= N; i++) {
+  adjacencyList[i].sort((a, b) => b - a);
+}
+
+const visited = new Array(N + 1).fill(false);
+const vertexDepths = new Array(N + 1).fill(-1);
+const orderOfVisit = new Array(N + 1).fill(0);
+let order = 1;
+
+function dfs(vertex, depth) {
+  visited[vertex] = true;
+  vertexDepths[vertex] = depth;
+  orderOfVisit[vertex] = order;
+  order += 1;
+
+  for (const neighbor of adjacencyList[vertex]) {
+    if (!visited[neighbor]) {
+      dfs(neighbor, depth + 1);
+    }
+  }
+}
+
+dfs(R, 0);
+
+let result = 0;
+
+for (let i = 1; i <= N; i++) {
+  result += vertexDepths[i] * orderOfVisit[i];
+}
+
+console.log(result);
