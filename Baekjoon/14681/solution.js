6b19825ce9bfa76c4fec09524b6b2da3043014df
devjaeyoon <devjaeyoon@gmail.com>
2025-04-08 09:56:09 +0900

    feat(Baekjoon): solve problem 14681 - 사분면 고르기

---
+++ Baekjoon/14681/solution.js
@@ -0,0 +1,16 @@
+const [x, y] = require('fs')
+  .readFileSync(0, 'utf-8')
+  .toString()
+  .trim()
+  .split('\n')
+  .map(Number);
+
+if (x > 0 && y > 0) {
+  console.log(1);
+} else if (x < 0 && y > 0) {
+  console.log(2);
+} else if (x < 0 && y < 0) {
+  console.log(3);
+} else {
+  console.log(4);
+}
