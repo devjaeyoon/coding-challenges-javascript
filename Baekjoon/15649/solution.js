dd7fadc4295f13836e0ee0cd4ad1cbab606fb07a
devjaeyoon <devjaeyoon@gmail.com>
2025-05-29 12:33:56 +0900

    feat(Baekjoon): solve problem 15649 - N과 M (1)

---
+++ Baekjoon/15649/solution.js
@@ -0,0 +1,30 @@
+const [N, M] = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
+
+const sequence = [];
+const visited = new Array(N + 1).fill(false);
+
+function generateSequence(selectedCount, startIndex) {
+  if (selectedCount === M) {
+    console.log(sequence.join(' '));
+    return;
+  }
+
+  for (let i = startIndex; i <= N; i++) {
+    if (visited[i]) {
+      continue;
+    }
+
+    sequence.push(i);
+    visited[i] = true;
+    generateSequence(selectedCount + 1, startIndex);
+    sequence.pop();
+    visited[i] = false;
+  }
+}
+
+generateSequence(0, 1);
