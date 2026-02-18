ad1feb241fbb9ac3d79bed1582b6b393b9c4e50f
devjaeyoon <devjaeyoon@gmail.com>
2026-02-18 11:37:14 +0900

    feat(Baekjoon): solve problem 25288 - 영어 시험

---
+++ Baekjoon/25288/solution.js
@@ -0,0 +1,10 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'example.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+const N = Number(input[0]);
+const alphabet = input[1];
+
+console.log(alphabet.repeat(N));
