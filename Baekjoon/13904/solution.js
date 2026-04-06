55d798e5914a0dbdc4e281184f35e2a5773e2cec
devjaeyoon <devjaeyoon@gmail.com>
2026-04-07 08:44:43 +0900

    feat(Baekjoon): solve problem 13904 - 과제

---
+++ Baekjoon/13904/solution.js
@@ -0,0 +1,109 @@
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  swap(idx1, idx2) {
+    [this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]];
+  }
+
+  push(value) {
+    this.heap.push(value);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let currentIndex = this.heap.length - 1;
+
+    while (currentIndex > 0) {
+      const parentIndex = Math.floor((currentIndex - 1) / 2);
+
+      if (this.heap[parentIndex] <= this.heap[currentIndex]) {
+        break;
+      }
+
+      this.swap(currentIndex, parentIndex);
+      currentIndex = parentIndex;
+    }
+  }
+
+  pop() {
+    if (this.heap.length === 1) {
+      return this.heap.pop();
+    }
+
+    const minValue = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.bubbleDown(0);
+
+    return minValue;
+  }
+
+  bubbleDown(index) {
+    const leftChildIndex = 2 * index + 1;
+    const rightChildIndex = 2 * index + 2;
+    const length = this.heap.length;
+    let smallestIndex = index;
+
+    if (
+      leftChildIndex < length &&
+      this.heap[leftChildIndex] < this.heap[smallestIndex]
+    ) {
+      smallestIndex = leftChildIndex;
+    }
+    if (
+      rightChildIndex < length &&
+      this.heap[rightChildIndex] < this.heap[smallestIndex]
+    ) {
+      smallestIndex = rightChildIndex;
+    }
+
+    if (smallestIndex !== index) {
+      this.swap(index, smallestIndex);
+      this.bubbleDown(smallestIndex);
+    }
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+}
+
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'example.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+const N = Number(input[0]);
+const assignments = [];
+
+for (let i = 1; i <= N; i++) {
+  const [d, w] = input[i].trim().split(/\s+/).map(Number);
+  assignments.push({ d, w });
+}
+
+assignments.sort((a, b) => a.d - b.d);
+
+const minHeap = new MinHeap();
+
+for (let i = 0; i < N; i++) {
+  const currentAssignment = assignments[i];
+
+  minHeap.push(currentAssignment.w);
+
+  if (minHeap.size() > currentAssignment.d) {
+    minHeap.pop();
+  }
+}
+
+let maxScore = 0;
+while (minHeap.size() > 0) {
+  maxScore += minHeap.pop();
+}
+
+console.log(maxScore);
