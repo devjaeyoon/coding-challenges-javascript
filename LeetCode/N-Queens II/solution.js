0f17ece6a6e23b78a13dfdf95674e8189eeead46
devjaeyoon <devjaeyoon@gmail.com>
2026-06-19 16:46:50 +0900

    feat(LeetCode): solve problem - N-Queens II

---
+++ LeetCode/N-Queens II/solution.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var totalNQueens = function (n) {
+  let count = 0;
+
+  const cols = new Set();
+  const posDiag = new Set();
+  const negDiag = new Set();
+
+  const backtrack = (row) => {
+    if (row === n) {
+      count++;
+      return;
+    }
+
+    for (let col = 0; col < n; col++) {
+      if (cols.has(col) || posDiag.has(row + col) || negDiag.has(row - col)) {
+        continue;
+      }
+
+      cols.add(col);
+      posDiag.add(row + col);
+      negDiag.add(row - col);
+
+      backtrack(row + 1);
+
+      cols.delete(col);
+      posDiag.delete(row + col);
+      negDiag.delete(row - col);
+    }
+  };
+
+  backtrack(0);
+
+  return count;
+};
