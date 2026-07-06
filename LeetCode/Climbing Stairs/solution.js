040ab496cf7be6f8e275ec6aad15c6bf229e5501
devjaeyoon <devjaeyoon@gmail.com>
2026-07-07 08:24:48 +0900

    feat(LeetCode): solve problem - Climbing Stairs

---
+++ LeetCode/Climbing Stairs/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  if (n === 1 || n === 2) {
+    return n;
+  }
+
+  let prev2 = 1;
+  let prev1 = 2;
+  let current = 0;
+
+  for (let i = 3; i <= n; i++) {
+    current = prev1 + prev2;
+    prev2 = prev1;
+    prev1 = current;
+  }
+
+  return current;
+};
