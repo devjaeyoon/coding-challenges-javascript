66ae13921cfaef0463d693e0b0ff338cd53b6c98
devjaeyoon <devjaeyoon@gmail.com>
2026-07-27 08:24:04 +0900

    feat(LeetCode): solve problem - Subsets II

---
+++ LeetCode/Subsets II/solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function (nums) {
+  nums.sort((a, b) => a - b);
+  const result = [];
+
+  function backtrack(start, currentSubset) {
+    result.push([...currentSubset]);
+
+    for (let i = start; i < nums.length; i++) {
+      if (i > start && nums[i] === nums[i - 1]) {
+        continue;
+      }
+
+      currentSubset.push(nums[i]);
+      backtrack(i + 1, currentSubset);
+      currentSubset.pop();
+    }
+  }
+
+  backtrack(0, []);
+
+  return result;
+};
