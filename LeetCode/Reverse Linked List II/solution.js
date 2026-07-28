7fdee930ddca70fdeecae1b906234068a3fe101f
devjaeyoon <devjaeyoon@gmail.com>
2026-07-29 08:19:20 +0900

    feat(LeetCode): solve problem - Reverse Linked List II

---
+++ LeetCode/Reverse Linked List II/solution.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function (head, left, right) {
+  if (!head || left === right) {
+    return head;
+  }
+
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let pre = dummy;
+
+  for (let i = 0; i < left - 1; i++) {
+    pre = pre.next;
+  }
+
+  let cur = pre.next;
+
+  for (let i = 0; i < right - left; i++) {
+    let nextNode = cur.next;
+    cur.next = nextNode.next;
+    nextNode.next = pre.next;
+    pre.next = nextNode;
+  }
+
+  return dummy.next;
+};
