dc03bdfba815d6dc0cadbfe1222055a3e11e45d7
devjaeyoon <devjaeyoon@gmail.com>
2026-08-08 14:38:28 +0900

    feat(LeetCode): solve problem - Binary Tree Zigzag Level Order Traversal

---
+++ LeetCode/Binary Tree Zigzag Level Order Traversal/solution.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function (root) {
+  if (!root) {
+    return [];
+  }
+
+  const result = [];
+  const queue = [root];
+  let isLeftToRight = true;
+
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+
+      if (isLeftToRight) {
+        currentLevel.push(node.val);
+      } else {
+        currentLevel.unshift(node.val);
+      }
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    result.push(currentLevel);
+    isLeftToRight = !isLeftToRight;
+  }
+
+  return result;
+};
