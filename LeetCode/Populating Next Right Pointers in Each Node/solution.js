29d67693209365dd710cbb3e23bb52a48a07cff7
devjaeyoon <devjaeyoon@gmail.com>
2026-08-21 08:27:08 +0900

    feat(LeetCode): solve problem - Populating Next Right Pointers in Each Node

---
+++ LeetCode/Populating Next Right Pointers in Each Node/solution.js
@@ -0,0 +1,39 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var connect = function (root) {
+  if (!root) {
+    return null;
+  }
+
+  let leftmost = root;
+
+  while (leftmost.left) {
+    let current = leftmost;
+
+    while (current) {
+      current.left.next = current.right;
+
+      if (current.next) {
+        current.right.next = current.next.left;
+      }
+
+      current = current.next;
+    }
+
+    leftmost = leftmost.left;
+  }
+
+  return root;
+};
