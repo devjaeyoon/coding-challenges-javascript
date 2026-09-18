efc573cb3e2975e6bab33618c38d74e03125fd16
devjaeyoon <devjaeyoon@gmail.com>
2026-09-19 01:04:22 +0900

    feat(LeetCode): solve problem - Binary Tree Postorder Traversal

---
+++ LeetCode/Binary Tree Postorder Traversal/solution.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root) {
+  const result = [];
+  const stack = [];
+
+  if (root) {
+    stack.push(root);
+  }
+
+  while (stack.length) {
+    const node = stack.pop();
+
+    result.push(node.val);
+
+    if (node.left) {
+      stack.push(node.left);
+    }
+    if (node.right) {
+      stack.push(node.right);
+    }
+  }
+
+  return result.reverse();
+};
