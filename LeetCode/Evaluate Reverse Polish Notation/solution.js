78e99dd03a24192eef0513d3936096975768b687
devjaeyoon <devjaeyoon@gmail.com>
2026-09-24 17:53:57 +0900

    feat(LeetCode): solve problem - Evaluate Reverse Polish Notation

---
+++ LeetCode/Evaluate Reverse Polish Notation/solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+  const stack = [];
+
+  for (const token of tokens) {
+    if (token === '+' || token === '-' || token === '*' || token === '/') {
+      const right = stack.pop();
+      const left = stack.pop();
+
+      if (token === '+') {
+        stack.push(left + right);
+      } else if (token === '-') {
+        stack.push(left - right);
+      } else if (token === '*') {
+        stack.push(left * right);
+      } else {
+        stack.push(Math.trunc(left / right));
+      }
+    } else {
+      stack.push(Number(token));
+    }
+  }
+
+  return stack[0];
+};
